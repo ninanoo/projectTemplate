2e8c3537880fbfa5e9195f1a0672cc1027bb489c
zxc <zxc@ninano.ga>
2017-09-05 04:58:35 +0900

    sync with master, remove .python.property, fix etc/install_all_pythons.sh

--- etc/install_all_pythons.sh
+++ etc/install_all_pythons.sh
@@ -1,21 +1,40 @@
-[ $? -eq 0 ] && rm -rf Python-3.6.2
-[ $? -eq 0 ] && tar Jxvf Python-3.6.2.tar.xz
-[ $? -eq 0 ] && cd Python-3.6.2
-[ $? -eq 0 ] && ./configure --prefix="$(readlink -f ../..)/$(basename `pwd` | tr '[A-Z]' '[a-z]')" --enable-shared
-[ $? -eq 0 ] && make && make altinstall
-[ $? -eq 0 ] && cd ..
+#!/bin/sh
 
-[ $? -eq 0 ] && rm -rf Python-3.5.4
-[ $? -eq 0 ] && tar Jxvf Python-3.5.4.tar.xz
-[ $? -eq 0 ] && cd Python-3.5.4
-[ $? -eq 0 ] && ./configure --prefix="$(readlink -f ../..)/$(basename `pwd` | tr '[A-Z]' '[a-z]')" --enable-shared
-[ $? -eq 0 ] && make && make altinstall
-[ $? -eq 0 ] && cd ..
 
-[ $? -eq 0 ] && rm -rf Python-2.7.13
-[ $? -eq 0 ] && tar Jxvf Python-2.7.13.tar.xz
-[ $? -eq 0 ] && cd Python-2.7.13
-[ $? -eq 0 ] && ./configure --prefix="$(readlink -f ../..)/$(basename `pwd` | tr '[A-Z]' '[a-z]')" --enable-shared
-[ $? -eq 0 ] && make && make altinstall
-[ $? -eq 0 ] && cd ..
+
+_BASE_PATH_=$(dirname `readlink -f $BASH_SOURCE`)
+
+cd $_BASE_PATH_			; [ $? -ne 0 ] && exit $?
+
+_BASE_PATH_=$(dirname $_BASE_PATH_)
+
+IFS=$'\n'
+
+for _FILE_ in $(ls -1 | grep -i -e "^Python.*\.tar\.xz$")
+do
+	_PATH_=$(expr "$_FILE_" : '\(.*\).......')
+
+	cd $_BASE_PATH_		; [ $? -ne 0 ] && exit $?
+	rm -rf $_PATH_		; [ $? -ne 0 ] && exit $?
+	cd -				; [ $? -ne 0 ] && exit $?
+	rm -rf $_PATH_		; [ $? -ne 0 ] && exit $?
+
+	tar Jxvf $_FILE_	; [ $? -ne 0 ] && exit $?
+
+	cd $_PATH_			; [ $? -ne 0 ] && exit $?
+
+	./configure --prefix="$_BASE_PATH_/$(echo $_PATH_ | tr '[A-Z]' '[a-z]')" --enable-shared	; [ $? -ne 0 ] && exit $?
+
+	make				; [ $? -ne 0 ] && exit $?
+	make altinstall		; [ $? -ne 0 ] && exit $?
+
+	cd -				; [ $? -ne 0 ] && exit $?
+done
+
+
+
+exit 0
+
+
+
 
