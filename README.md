a8947430d25fa205a89983c0748080ce1945d508
zxc <zxc@ninano.ga>
2017-09-06 23:34:23 +0900

    minor fix

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # projectTemplate - pythonProjectTemplate - outSourcingBoard
 pythonProjectTemplate for 'outSourcingBoard' python project
+
+TODO: readlink
--- etc/install_all_pythons.sh
+++ etc/install_all_pythons.sh
@@ -1,5 +1,32 @@
 #!/bin/sh
 
+#set -x
+
+
+
+_PATH_TAIL_=""
+_FILE_LIST_=""
+
+for _FILE_
+do
+	case $_FILE_ in
+		--tail=*) _PATH_TAIL_=`echo "$_FILE_" | sed 's/^--tail=//'`
+				  ;;
+		*) if [ ! `echo "$_FILE_" | grep "..*\.tar\...*"` ]
+		   then
+			   echo "error: invalid tar archive $_FILE_"
+			   exit 1
+		   fi
+		   _FILE_LIST_=$_FILE_LIST_${_FILE_LIST_:+ }$_FILE_
+		   ;;
+	esac
+done
+
+if [ ! "$_FILE_LIST_" ]
+then
+	_FILE_LIST_=`ls Python*.tar.*`
+fi
+
 
 
 _BASE_PATH_=$(dirname `readlink -f $BASH_SOURCE`)
@@ -8,22 +35,21 @@ cd $_BASE_PATH_			; [ $? -ne 0 ] && exit $?
 
 _BASE_PATH_=$(dirname $_BASE_PATH_)
 
-IFS=$'\n'
-
-for _FILE_ in $(ls -1 | grep -i -e "^Python.*\.tar\.xz$")
+for _FILE_ in $_FILE_LIST_
 do
-	_PATH_=$(expr "$_FILE_" : '\(.*\).......')
+	_SRC_PATH_=`echo "$_FILE_" | sed 's/\.tar\..*$//'`
+	_DST_PATH_=$_SRC_PATH_$_PATH_TAIL_
 
 	cd $_BASE_PATH_		; [ $? -ne 0 ] && exit $?
-	rm -rf $_PATH_		; [ $? -ne 0 ] && exit $?
+	rm -rf $_DST_PATH_	; [ $? -ne 0 ] && exit $?
 	cd -				; [ $? -ne 0 ] && exit $?
-	rm -rf $_PATH_		; [ $? -ne 0 ] && exit $?
+	rm -rf $_SRC_PATH_	; [ $? -ne 0 ] && exit $?
 
-	tar Jxvf $_FILE_	; [ $? -ne 0 ] && exit $?
+	tar axvf $_FILE_	; [ $? -ne 0 ] && exit $?
 
-	cd $_PATH_			; [ $? -ne 0 ] && exit $?
+	cd $_SRC_PATH_		; [ $? -ne 0 ] && exit $?
 
-	./configure --prefix="$_BASE_PATH_/$(echo $_PATH_ | tr '[A-Z]' '[a-z]')" --enable-shared	; [ $? -ne 0 ] && exit $?
+	./configure --prefix="$_BASE_PATH_/$_DST_PATH_" --enable-shared	; [ $? -ne 0 ] && exit $?
 
 	make				; [ $? -ne 0 ] && exit $?
 	make altinstall		; [ $? -ne 0 ] && exit $?
