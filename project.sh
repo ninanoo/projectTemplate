fef38ccc632813d93f09bc32d0de7232b9b28056
zxc <zxc@ninano.ga>
2018-03-05 16:24:35 +0900

    add man related and several fixes

---
+++ project.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+
+
+echo "    PROJECTHOME: $PROJECTHOME"
+echo
+echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
+echo "           PATH: $PATH"
+echo "        MANPATH: $MANPATH"
+
+
+
+
+exit 0
+
+
+
+
